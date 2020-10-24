f1c52565c73539ba2539306c0fbb8aad07d31599
nafan <https://github.com/syah255>
2020-10-24 17:35:06 +0000

    commit for lab1

---
+++ userinput.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+/*This is my second program in C */
+int age;
+printf("Hi, How old are you");
+scanf("%d", &age);
+printf("You are %d years old",age);
+return (0);
+}
